11233bedd523483d3317ece5e22767b1d8bebe46
Adrian Woźniak <adrian.wozniak@ita-prog.pl>
2022-06-15 16:21:20 +0200

    Clippy suggestions

--- scripts/zip-ci.sh
+++ scripts/zip-ci.sh
@@ -1,9 +1,57 @@
-echo Building binaries-$1.zip
+echo Building binaries for $1
 
-zip binaries-$1.zip ./target/x86_64-unknown-linux-musl/release/amdfand;
-zip binaries-$1.zip ./target/x86_64-unknown-linux-musl/release/amdmond;
-zip binaries-$1.zip ./target/x86_64-unknown-linux-musl/release/amdvold;
-zip binaries-$1.zip ./target/x86_64-unknown-linux-musl/release/amdgui-helper;
-zip binaries-$1.zip ./target/amdguid-wayland.zip;
-zip binaries-$1.zip ./target/amdguid-glium.zip
-zip binaries-$1.zip ./target/amdguid-glow.zip
+cd "$(git rev-parse --show-toplevel)"
+
+ROOT="$(git rev-parse --show-toplevel)"
+
+./scripts/build.sh
+
+function build_tag_gz() {
+  zip_name=$1
+  cd ${ROOT}
+  for name in $*; do
+    cp ${ROOT}/target/x86_64-unknown-linux-musl/release/${name} ${ROOT}/tmp
+    cp ${ROOT}/services/${name}.service ./tmp
+
+    cd ${ROOT}/tmp
+    tar -cvf ${zip_name}.tar.gz ${name}.service ${name}
+    cd ${ROOT}
+  done
+
+  cd ${ROOT}/tmp
+  for name in $*; do
+    rm ${name}.service ${name}
+  done
+  cd ${ROOT}
+}
+
+function tar_gui() {
+  tar_name=$1
+
+  cd ${ROOT}/tmp
+  unzip ${tar_name}.zip
+
+  cp ${ROOT}/target/x86_64-unknown-linux-musl/release/amdgui-helper ${ROOT}/tmp
+  cp ${ROOT}/services/amdgui-helper.service ${ROOT}/tmp
+  tar -cvf ${tar_name}.tar.gz amdgui-helper amdguid amdgui-helper.service
+}
+
+build_tag_gz amdfand
+build_tag_gz amdmond
+build_tag_gz amdvold
+
+tar_gui amdguid-wayland
+tar_gui amdguid-glium
+tar_gui amdguid-glow
+
+cd ${ROOT}/tmp
+
+for f in $(ls *.tar.gz); do
+  md5sum $f
+done
+
+cd ${ROOT}/tmp
+for file in $(ls *.tar.gz);
+do
+  mv $file ${ROOT}/${1}-$file
+done
