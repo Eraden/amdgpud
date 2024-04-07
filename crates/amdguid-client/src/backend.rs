2514f7558f20cf54a8daa2bedce91fd5c26ef5ca
Adrian Woźniak <adrian.wozniak@ita-prog.pl>
2024-04-07 11:48:33 +0200

    Fix frozen UI

--- crates/amdguid-client/src/backend.rs
+++ crates/amdguid-client/src/backend.rs
@@ -7,7 +7,9 @@ use tokio::sync::mpsc::UnboundedReceiver;
 pub fn run_app(amd_gui: Arc<Mutex<AmdGui>>, _receiver: UnboundedReceiver<bool>) {
     eframe::run_native(
         "Amd GPU Client",
-        eframe::NativeOptions::default(),
+        eframe::NativeOptions {
+            ..Default::default()
+        },
         Box::new(|_cc| Box::new(MyApp { amd_gui })),
     )
     .expect("AMD GUID failed");
@@ -20,5 +22,6 @@ struct MyApp {
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         amdguid::backend::create_ui(self.amd_gui.clone(), ctx);
+        ctx.request_repaint_after(std::time::Duration::from_millis(1000 / 60));
     }
 }
